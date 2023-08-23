a779f2318295311bd07d3f2823bc7b49ffd116bf
Mark Ciubal <mark.ciubal@gmail.com>
2023-08-22 20:46:32 -0700

    Working with useEffec.t

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 import { useEffect } from 'react';
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+
+
 
 function App() {
+  useEffect(() => {
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  }, []);
+
   return (
     <div className="App">
       <div id="map" className={"map"}></div>
