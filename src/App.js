7dc808ae0557c7cdb9f0aca18e9cb02c769422a5
Mark Ciubal <mark.ciubal@gmail.com>
2023-08-24 11:31:02 -0700

    Working map, point on map. Working on getting zero and center functions working, but they are not triggering a map update.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import './style.css';
 import MainMap from './components/MainMap';
-import ControlPanel from './components/ControlPanel.js';
 
 function App() {
   return (
     <div className="App box">
-      <header>
-        <ControlPanel />
-      </header>
       <section>
         <MainMap/>
       </section>
