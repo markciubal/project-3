64f3b6808e063c1145965dc6ce17ec92d2685ff7
byronallan <127366720+byronallan@users.noreply.github.com>
2023-08-24 21:03:34 -0700

    new branch for login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './style.css';
 import MainMap from './components/MainMap';
 import ControlPanel from './components/ControlPanel.js';
 
+import Login from './pages/Login';
+// import Signup from './pages/Signup';
+
 function App() {
   return (
     <div className="App box">
       <header>
         <ControlPanel />
       </header>
+      <li>
+          <Link to="/login">
+              Login
+          </Link>
+      </li>
       <section>
         <MainMap/>
       </section>
