06723d8d10ef578109b96e33c12af11b0429ad2f
Mark Ciubal <mark.ciubal@gmail.com>
2023-08-31 22:48:09 -0700

    Stripped down.

--- client/src/components/MainMap.js
+++ client/src/components/MainMap.js
@@ -1,4 +1,6 @@
-import React, { Component, useEffect, useState, useRef, useCallback  } from "react";
+import * as React from 'react';
+import Map from 'react-map-gl';
+
 import '../App.css';
 import ControlPanel from "./ControlPanel";
 import { useMutation, useQuery } from '@apollo/client';
@@ -16,7 +18,6 @@ import { useGeolocated } from "react-geolocated";
 import Login from './Login';
 import SignUp from './SignUp';
 import postToGeoJSON from '../utils/postToGeoJSON';
-import "ol/ol.css";
 
 // Queries and Mutations
 import { GET_ALL_POSTS } from '../utils/queries';
@@ -57,12 +58,12 @@ const MainMap = () => {
   // Map states.
   // Paired in [ LONGITUDE, LATITUDE ] because OpenLayers uses the pair (Longitude before Latitude, i.e., toLonLat, fromLonLat).
   const GEOGRAPHIC_CENTER_OF_UNITED_STATES = [-103.771556, 44.967243];
-  const [coordinateRoundTo, setCoordinateRoundTo] = useState(3);
-  const [view, setView] = useState({ center: fromLonLat(GEOGRAPHIC_CENTER_OF_UNITED_STATES), zoom: 3, extent: [-20037508.34, 20037508.34, 20037508.34, -20037508.34] });
-  const [centerLatitude, setCenterLatitude] = useState(GEOGRAPHIC_CENTER_OF_UNITED_STATES[0]);
-  const [centerLongitude, setCenterLongitude] = useState(GEOGRAPHIC_CENTER_OF_UNITED_STATES[1]);
-  const [currentEmoji, setCurrentEmoji] = useState('▼');
-  const [postGeoJSON, setPostGeoJSON] = useState(new GeoJSON({
+  const [coordinateRoundTo, setCoordinateRoundTo] = React.useState(3);
+  const [view, setView] = React.useState({ center: fromLonLat(GEOGRAPHIC_CENTER_OF_UNITED_STATES), zoom: 3, extent: [-20037508.34, 20037508.34, 20037508.34, -20037508.34] });
+  const [centerLatitude, setCenterLatitude] = React.useState(GEOGRAPHIC_CENTER_OF_UNITED_STATES[0]);
+  const [centerLongitude, setCenterLongitude] = React.useState(GEOGRAPHIC_CENTER_OF_UNITED_STATES[1]);
+  const [currentEmoji, setCurrentEmoji] = React.useState('▼');
+  const [postGeoJSON, setPostGeoJSON] = React.useState(new GeoJSON({
     featureProjection: "EPSG:3857",
   }).readFeatures({
     "type": "FeatureCollection",
@@ -72,28 +73,27 @@ const MainMap = () => {
     "features": []
   }));
   // Bottom sliding pane state.
-  const [isPostPaneOpen, setIsPostPaneOpen] = useState(false);
-  const [isSignUpPaneOpen, setIsSignUpPaneOpen] = useState(false);
-  const [isLoginPaneOpen, setIsLoginPaneOpen] = useState(false);
-  const [isSelectedPaneOpen, setIsSelectedPaneOpen] = useState(false);
+  const [isPostPaneOpen, setIsPostPaneOpen] = React.useState(false);
+  const [isSignUpPaneOpen, setIsSignUpPaneOpen] = React.useState(false);
+  const [isLoginPaneOpen, setIsLoginPaneOpen] = React.useState(false);
+  const [isSelectedPaneOpen, setIsSelectedPaneOpen] = React.useState(false);
 
   const geoJSONString = JSON.stringify();
 
   //For clustering posts.
-  const [distance, setDistance] = useState(50);
-  const [selected, setSelected] = useState("Click a cluster for details");
+  const [distance, setDistance] = React.useState(50);
+  const [selected, setSelected] = React.useState("Click a cluster for details");
 
   // For managing the user's current position.
-  const [currentLatitude, setCurrentLatitude] = useState();
-  const [currentLongitude, setCurrentLongitude] = useState();
-  const [accuracy, setAccuracy] = useState();
+  const [currentLatitude, setCurrentLatitude] = React.useState();
+  const [currentLongitude, setCurrentLongitude] = React.useState();
+  const [accuracy, setAccuracy] = React.useState();
 
   // For managing user clicks and date.
-  const [selectedMapPosts, setSelectedMapPosts] = useState([]);
+  const [selectedMapPosts, setSelectedMapPosts] = React.useState([]);
 
   // Getting posts.
   const { loading, error, data } = useQuery(GET_ALL_POSTS);
-  const earthquakeLayer = useRef();
 
   const panAndZoomToMe = async () => {
     function showError(error) {
@@ -128,14 +128,14 @@ const MainMap = () => {
 
 
 
-  useEffect(() => {
+  React.useEffect(() => {
     let viewCenter = toLonLat(view.center);
     setCenterLatitude(viewCenter[1]);
     setCenterLongitude(viewCenter[0]);
   }, [view]);
 
   
-  useEffect(() => {
+  React.useEffect(() => {
     async function getAllPosts() {
     if (!loading) {
       console.log(postGeoJSON);
@@ -162,127 +162,6 @@ const MainMap = () => {
         setIsSignUpPaneOpen={setIsSignUpPaneOpen}
         panAndZoomToMe={panAndZoomToMe}
       />
-      <RMap
-        className="map"
-        initial={view}
-        view={[view, setView]}
-        noDefaultControls={true}
-      >
-        <ROSM />
-        <RControl.RAttribution />
-        <RControl.RRotate />
-        <RLayerVector>
-          <RFeature geometry={new Point(fromLonLat([centerLongitude, centerLatitude]))} >
-            <RStyle.RStyle>
-              
-              <RStyle.RCircle radius={5}>
-                {/* <RStyle.RFill color="#F8F7F4" /> */}
-                <RStyle.RStroke width="1" color="darkred" />
-              </RStyle.RCircle>
-            </RStyle.RStyle>
-          </RFeature>
-          <RFeature geometry={new Point(fromLonLat([centerLongitude, centerLatitude]))} >
-            <RStyle.RStyle>
-              
-              <RStyle.RCircle radius={10}>
-                {/* <RStyle.RFill color="#F8F7F4" /> */}
-                <RStyle.RStroke width="2" color="darkred" />
-              </RStyle.RCircle>
-            </RStyle.RStyle>
-          </RFeature>
-          
-          <RLayerCluster
-            ref={earthquakeLayer}
-            distance={distance}
-            format={new GeoJSON({ featureProjection: "EPSG:3857" })}
-            features={postGeoJSON}
-            // url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
-            onClick={React.useCallback((e) => {
-              const features = e.target.get("features") ?? [];
-              // console.log(features);
-              setSelectedMapPosts(features);
-              setIsSelectedPaneOpen(true);
-              // setSelected(
-              //   `${features.length} earthquakes in this location, ` +
-              //     `magnitudes are ${features
-              //       .map((eq) => eq.get("mag"))
-              //       .join(", ")}`
-              // );
-            }, [])}
-          >
-          <RStyle.RStyle
-            cacheSize={1024}
-            cacheId={useCallback(
-              (feature, resolution) =>
-                // This is the hashing function, it takes a feature as its input
-                // and returns a string
-                // It must be dependant of the same inputs as the rendering function
-                feature.get("features").length > 1
-                  ? "#" + extentFeatures(feature.get("features"), resolution)
-                  : "$" + radiusStar(feature.get("features")[0]),
-              []
-            )}
-            render={useCallback((feature, resolution) => {
-              // This is the rendering function
-              // It has access to the cluster which appears as a single feature
-              // and has a property with an array of all the features that make it
-              const size = feature.get("features").length;
-              // This is the size (number of features) of the cluster
-              if (size > 1) {
-                // Render a blob with a number
-                const radius = extentFeatures(
-                  feature.get("features"),
-                  resolution
-                );
-                return (
-                  // A dynamic style should return a fragment instead of a
-                  // full-blown RStyle - returning a full RStyle here
-                  // will simply replace the style used by the vector layer
-                  // with a fixed one
-                  <React.Fragment>
-                    <RCircle radius={radius}>
-                      <RFill color={colorBlob(size)} />
-                      <RStroke color="rgba(0, 0, 0, 0.6)" width={3} />
-                    </RCircle>
-                    <RText text={size.toString()}>
-                      <RFill color="#fff" />
-                      <RStroke color="rgba(0, 0, 0, 0.6)" width={5} />
-                    </RText>
-                  </React.Fragment>
-                );
-              }
-              // We have a single feature cluster
-              const unclusteredFeature = feature.get("features")[0];
-              const body = unclusteredFeature.get("body");
-              // console.log(mag);
-              return (
-                <>
-                  <RCircle radius={10}>
-                    <RFill color="rgba(255, 255, 255, 0.8)" />
-                    <RStroke color="rgba(0, 0, 0, 1)" width={1.5} />
-                  </RCircle>
-                  <RText text={body}>
-                    <RFill color="#000" />
-                    <RStroke color="rgba(255, 255, 255, 0.6)" width={5} />
-                  </RText>
-                </>
-              );
-            }, [])}
-          />
-        </RLayerCluster>
-        </RLayerVector>
-      </RMap>
-      <CenterMenu
-        centerLatitude={centerLatitude}
-        centerLongitude={centerLongitude}
-        coordinateRoundTo={coordinateRoundTo}
-        isPostPaneOpen={isPostPaneOpen}
-        setIsPostPaneOpen={setIsPostPaneOpen}
-        isLoginPaneOpen={isLoginPaneOpen}
-        setIsLoginPaneOpen={setIsLoginPaneOpen}
-        currentEmoji={currentEmoji}
-        panAndZoomToMe={panAndZoomToMe}
-      />    
       <SlidingPane
         closeIcon={<div>Close</div>}
         className="bottom-pane"
