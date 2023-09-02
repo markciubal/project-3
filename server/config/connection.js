6eaa33272ce9606b1b4c6132927ddca0451d8f5d
Mark Ciubal <mark.ciubal@gmail.com>
2023-09-02 11:55:53 -0700

    Working build?

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://markciubal:ptKfsx._EGZ4Squ@cluster0.iv41rrc.mongodb.net/pinpoint?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/pinpoint');
+mongoose.connect("mongodb+srv://markciubal:ptKfsx._EGZ4Squ@cluster0.iv41rrc.mongodb.net/pinpoint?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/pinpoint', {
+    socketTimeoutMS: 60000
+});
 
 module.exports = mongoose.connection;
