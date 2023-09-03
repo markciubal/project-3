1b091b60b8029c353017251308761c8e60e54557
Mark Ciubal <mark.ciubal@gmail.com>
2023-09-02 22:48:37 -0700

    Added . to server.js file.

--- server/server.js
+++ server/server.js
@@ -18,14 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, './client/build/images')));
+app.use('/images', express.static(path.join(__dirname, '../client/build/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/build/')));
+  app.use(express.static(path.join(__dirname, '../client/build/')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
