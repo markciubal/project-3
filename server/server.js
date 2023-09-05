9a6b7f77e4a3393277a239a053d05e4953daab52
Mark Ciubal <mark.ciubal@gmail.com>
2023-09-04 20:09:51 -0700

    Debugging database not longer connecting.

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-console.log(port);
+console.log(PORT);
 const app = express();
 const server = new ApolloServer({
   typeDefs,
